6e57db34e0258ccacab10ebbc8e3d4a5812e6dce
Packman700 <DominikWlodarczyk@poczta.onet.pl>
2020-05-05 15:10:05 +0200

    Final version v.1 # Randomize len key

--- encrypting.py
+++ encrypting.py
@@ -1,3 +1,6 @@
+import encryption_key
+import random
+
 def letter2num(message, key):
     if len(message) % 2 == 1:
         message += ' '
@@ -19,12 +22,23 @@ def num_multiplication(message):
                 last2num[2] = multiplication_and_key(last2num[0], last2num[1], last2num[0])
             elif index % 4 == 3:
                 last2num[2] = multiplication_and_key(last2num[0], last2num[1], last2num[1])
-            multiplication_len.append(len(last2num[2]))
+            multiplication_len.append(str(len(last2num[2])))
             multiplication_var = multiplication_var + last2num[2]
     encrypted = {"encrypted_message": multiplication_var, "length": multiplication_len}
     return encrypted
 
 
+def len_encryption(length):
+    out = ''
+    for char in length:
+        out = out + encryption_key.key_encryption_dictionary[char][random.randint(0, 19)]
+    return out
+#def num2letter (num):
+#   if type(num) == list:
+#       for i in num:
+
+
+
+
 def multiplication_and_key(first,second,key):
     return str(first*second)+str(key)
-
--- encryption_key.py
+++ encryption_key.py
@@ -3,12 +3,45 @@ import math
 import string
 
 
-def make_encryption_dictionary (seed):
+def make_encryption_dictionary(seed):
     # Generate alphabet and numbers dictionary with 0 argument
     dictionary = dict.fromkeys(string.ascii_lowercase, 0)
     for x in range(10):
         dictionary[str(x)] = 0
-    dictionary[' '] = 0
+    if True:  # This is just to organise code
+        dictionary['!'] = 0
+        dictionary['@'] = 0
+        dictionary['#'] = 0
+        dictionary['$'] = 0
+        dictionary['%'] = 0
+        dictionary['^'] = 0
+        dictionary['&'] = 0
+        dictionary['*'] = 0
+        dictionary['('] = 0
+        dictionary[')'] = 0
+        dictionary['-'] = 0
+        dictionary['_'] = 0
+        dictionary['='] = 0
+        dictionary['+'] = 0
+        dictionary['{'] = 0
+        dictionary['}'] = 0
+        dictionary['['] = 0
+        dictionary[']'] = 0
+        dictionary['|'] = 0
+        dictionary["\\"] = 0
+        dictionary[':'] = 0
+        dictionary[';'] = 0
+        dictionary['"'] = 0
+        dictionary["'"] = 0
+        dictionary['<'] = 0
+        dictionary['>'] = 0
+        dictionary[','] = 0
+        dictionary['.'] = 0
+        dictionary['/'] = 0
+        dictionary['?'] = 0
+        dictionary['~'] = 0
+        dictionary['`'] = 0
+        dictionary[' '] = 0
 
     # Generate seed
     random.seed(math.sin(seed / 0.153) * -(1536 ** 2))  # random calculations
@@ -26,3 +59,33 @@ def make_encryption_dictionary (seed):
         dictionary[key] = used_numbers[-1]
 
     return dictionary
+
+
+def make_reverse_dictionary(dictionary):
+    out = {}
+    for key, value in zip(dictionary.keys(), dictionary.values()):
+        out[value] = key
+    return out
+
+
+key_encryption_dictionary = {
+    '5': [',w?+ks]', 'Qv9ZxUl', 'M$2jL-#', '<s=W4Qg', 'hT/vQ}#',
+          'A-(h,wG', '>Q-XTlG', 'u^H*wO1', '0vIIOQD', 'ugF]y1$',
+          '.YJ)^1k', 'XM!SQYX', 'N%;%yYM', 'N^gIl>3', 'kd6wB*_',
+          'G7^u\]M', '0k@%n;=', 'mdU}iA8', '4LBO:yK', 'Q%4ma9g'],
+    ',w?+ks]': '5', 'Qv9ZxUl': '5', 'M$2jL-#': '5', '<s=W4Qg': '5',
+    'hT/vQ}#': '5', 'A-(h,wG': '5', '>Q-XTlG': '5', 'u^H*wO1': '5',
+    '0vIIOQD': '5', 'ugF]y1$': '5', '.YJ)^1k': '5', 'XM!SQYX': '5',
+    'N%;%yYM': '5', 'N^gIl>3': '5', 'kd6wB*_': '5', 'G7^u\]M': '5',
+    '0k@%n;=': '5', 'mdU}iA8': '5', '4LBO:yK': '5', 'Q%4ma9g': '5',
+
+    '6': ['[RG[O_h', '-!5J7Bu', 'SO7[y!v', 'chZh99f', 'l+8DMth',
+          '6a%or8)', 'Y@-=OpB', 'M*(;mmw', 'WWcH2Kb', '*DTb^WZ',
+          '9vs>@;A', 'n$)JYzh', '?KS)e#y', 'iYYejM}', '^SqdNjp',
+          '0MSin#*', '1%^J13:', 'K*NOXE-', 'v[nU]*1', 'Nz;B#Nb'],
+    '[RG[O_h': '6', '-!5J7Bu': '6', 'SO7[y!v': '6', 'chZh99f': '6',
+    'l+8DMth': '6', '6a%or8)': '6', 'Y@-=OpB': '6', 'M*(;mmw': '6',
+    'WWcH2Kb': '6', '*DTb^WZ': '6', '9vs>@;A': '6', 'n$)JYzh': '6',
+    '?KS)e#y': '6', 'iYYejM}': '6', '^SqdNjp': '6', '0MSin#*': '6',
+    '1%^J13:': '6', 'K*NOXE-': '6', 'v[nU]*1': '6', 'Nz;B#Nb': '6'
+}
